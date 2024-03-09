e6506374b6d3c7207912bac7e4810ea42c82eb84
Rolan Adarlo <adarlorolan14@gmail.com>
2024-03-10 06:42:16 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://portfolio-rolan.netlify.app/',
+  base: '/',
   plugins: [vue()],
 })
